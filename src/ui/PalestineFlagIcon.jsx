e5fd18a06741956839fd6f28c52ef22edde06464
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2024-11-04 21:29:39 +0200

    Account  UI page

---
+++ src/ui/PalestineFlagIcon.jsx
@@ -0,0 +1,12 @@
+import { SvgIcon } from "@mui/material";
+const PalestineFlagIcon = (props) => {
+  return (
+    <SvgIcon {...props} viewBox="0 0 24 16">
+      <rect width="24" height="16" fill="#007A33" />
+      <rect width="24" height="10.67" fill="#ffffff" />
+      <rect width="24" height="5.33" fill="#000000" />
+      <polygon points="0,0 0,16 9.67,8" fill="#E4002B" />
+    </SvgIcon>
+  );
+};
+export default PalestineFlagIcon;
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,21 +1,29 @@
 import { createTheme, responsiveFontSizes } from "@mui/material";
 
 const theme = responsiveFontSizes(
-    createTheme({
-        palette: {
-            primary: {
-                main: '#027e7b'
-            },
-            // secondary: {
-            // },
-            // third: {
-            // },
-
-        }, typography: {
-            fontFamily: ['Sora', 'Courier', 'monospace'].join(',')
-        }
-    })
+  createTheme({
+    palette: {
+      primary: {
+        main: "#027e7b",
+      },
+      // secondary: {
+      // },
+      // third: {
+      // },
+    },
+    typography: {
+      fontFamily: ["Sora", "Courier", "monospace"].join(","),
+    },
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 745,
+        md: 900,
+        lg: 1200,
+        xl: 1536,
+      },
+    },
+  })
 );
 
 export default theme;
-
