6e6d732295b41e22912d7d5c80756a2a9e8e3101
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2024-11-08 23:28:59 +0200

    hot-fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Suspense } from "react"
-import {RouterProvider } from "react-router-dom"
+import { RouterProvider } from "react-router-dom"
 import { Provider } from "react-redux"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ThemeProvider } from "@emotion/react"
 import { DialogsProvider, NotificationsProvider } from "@toolpad/core"
-
-// import Login from './pages/Login'
-// import Account from './pages/Account'
-// import PageNotFound from "./pages/PageNotFound"
-// import Dashboard from "./pages/Dashboard"
-// import Courses from "./pages/Courses"
-// import ForgotPassword from "./pages/ForgotPassword"
-// import ResetPassword from "./pages/ResetPassword"
-// import MyCertificates from "./pages/MyCertificates"
-// import MyCourses from "./pages/MyCourses"
-// import Users from "./pages/Users"
-// import Certificates from "./pages/Certificates"
 
 import SpinnerLoader from "./ui/SpinnerLoader.tsx"
 
@@ -27,7 +15,6 @@ import store from './store.js'
 import '@fontsource/sora'
 import queryClient from "./cache/queryClient.js"
 import router from "./utils/router.tsx"
-
 
 function App() {
 
--- src/pages/UnexpectedError.tsx
+++ src/pages/UnexpectedError.tsx
@@ -8,59 +8,26 @@ const UnexpectedError = ({
     error = {},
     resetErrorBoundary,
 }) => {
-    if (isProduction)
-        return (
-            <Stack gap={2} sx={{
-                justifyContent: "center",
-                alignItems: "center",
-            }}>
-                <Typography variant="h4" sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                }}>
-                    <ErrorOutlineIcon fontSize={"large"} sx={{ m: 10 }} /> Unexpected
-                    Error
-                </Typography>
-                <Button
-                    onClick={resetErrorBoundary}
-                    variant="contained"
-                    color="warning"
-                >
-                    Try again
-                </Button>
-            </Stack >
-        );
-
     return (
-        <Stack gap={2} role="alert" sx={{
+        <Stack gap={2} sx={{
             justifyContent: "center",
             alignItems: "center",
         }}>
-            <Typography variant="h4" fontWeight={500}>
-                Unexpected Error: {error?.message ?? JSON.stringify(error)}
+            <Typography variant="h4" sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+            }}>
+                <ErrorOutlineIcon fontSize={"large"} sx={{ m: 10 }} /> Unexpected
+                Error
             </Typography>
-            <Typography sx={{
-                fontSize: "1.2rem",
-                overflow: "auto",
-            }}>{error?.message}</Typography>
-            <Typography sx={{
-                fontSize: "1.2rem",
-                overflow: "auto",
-                padding: "15px",
-                border: "2px solid lightgrey",
-                borderRadius: "4px"
-            }}>{error?.stack}</Typography>
-            <Stack direction="row" sx={{ justifyContent: "center" }}>
-                <Button
-                    onClick={resetErrorBoundary}
-                    fullWidth={false}
-                    variant="contained"
-                    color="warning"
-                >
-                    Try again
-                </Button>
-            </Stack>
+            <Button
+                onClick={resetErrorBoundary}
+                variant="contained"
+                color="warning"
+            >
+                Try again
+            </Button>
         </Stack >
     );
 };
