1cb5e70d3485fc2795eec267be66e532cdde090d
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2024-11-16 16:44:15 +0200

    squeringCoures
    hot-changes

---
+++ src/pages/Dashboard/TryingComponents.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface TryingComponents {
+  // Define props here
+}
+
+const TryingComponents: React.FC<TryingComponents> = () => {
+  return <div>x</div>;
+};
+
+export default TryingComponents;
